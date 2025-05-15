c815e6296bf31a430a048da37b175fa22c628269
Kristjan <kristjanfurlan7@gmail.com>
2025-05-15 12:44:43 +0200

    SBP

---
+++ basic zanke.py
@@ -0,0 +1,34 @@
+#fruits = ("bana, jabi, hruk")
+
+#for a in fruits:
+#   print(fruits)
+
+#for i in range(7):
+ #   print(i)
+
+#i = 1
+#while i < 5:
+#    print(i)
+ #   i += 1
+
+#user_input = ""
+#while user_input != "exit":
+#    user_input = input("type \"exit\" to quit: ")
+
+#for i in range(11):
+#    print(i)
+#else:
+ #   print("Loop finished")
+
+#for i in range(2, 22, 2):
+#    print(i)
+
+#user_name = "Kristjan"
+#for letter in user_name:
+#    print(letter)
+
+#i = 5
+#while i >0:
+#    print(i)
+#    i -=1
+
