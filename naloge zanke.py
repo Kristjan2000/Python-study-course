bd4ef4f307d2fd4e17ce1656bf1414e66f88aafe
Kristjan <kristjanfurlan7@gmail.com>
2025-05-15 12:55:30 +0200

    fisrt commit

--- naloge zanke.py
+++ naloge zanke.py
@@ -1,27 +1,27 @@
-# user_input = ""
-# while user_input != "skrivnost":
-#     user_input = input("Vnesi geslo: ")
-#     if user_input == "skrivnost":
-#         print("dostop dovoljen")
-#     else:
-#         print("nimas pravic")
+user_input = ""
+while user_input != "skrivnost":
+    user_input = input("Vnesi geslo: ")
+    if user_input == "skrivnost":
+        print("dostop dovoljen")
+    else:
+        print("nimas pravic")
 
-# imena = ["Ana", "Borut", "Andraž", "Cene", "Ajda"]
-# for ime in imena:
-#     firstletter = ime[0]
-#     if firstletter == "A":
-#         print(ime)
+imena = ["Ana", "Borut", "Andraž", "Cene", "Ajda"]
+for ime in imena:
+    firstletter = ime[0]
+    if firstletter == "A":
+        print(ime)
 
-# poskusi = 0
-# user_input = ""
-# while user_input != "0":
-#     user_input = input("Vnesi naključno število do 20: ")
-#     if user_input != "0":
-#         poskusi += 1
-#         print("poskusi se enkrat")
-#     else:
-#         print("cestitke izbral si pravo")
-# print("Število poskusov: "poskusi)
+poskusi = 0
+user_input = ""
+while user_input != "0":
+    user_input = input("Vnesi naključno število do 20: ")
+    if user_input != "0":
+        poskusi += 1
+        print("poskusi se enkrat")
+    else:
+        print("cestitke izbral si pravo")
+print("Število poskusov: "poskusi)
 
 import random
 
